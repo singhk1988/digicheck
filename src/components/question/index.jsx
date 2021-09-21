58227269d45744ed7b7a46a5fec33fd66d765f84
Harman1997-96 <hs802686@gmail.com>
2021-09-21 22:36:54 +0530

    radioButton,CheckBox

--- src/components/question/index.jsx
+++ src/components/question/index.jsx
@@ -1,49 +1,15 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Button } from "antd";
-import Start from "../start";
-import Funktionen from "../funktionen_2/index";
-import questions from "../../Questions.json";
-function Question() {
-  // const [activeStep, setActiveStep] = useState(0)
-  // let body = ''
+import React from 'react'
+import SelectButton from '../selectButton'
+import RadioButton from '../radioButton/radioButton'
 
-  // if (activeStep === 0) {
-  //   body = <Start />
-  // } else if (activeStep === 1) {
-  //   body = <Funktionen />
-  // }
-  // const onClickContinue = () => {
-  //   const nextStep = activeStep + 1
-  //   setActiveStep(nextStep < 8 ? nextStep : 7)
-  // }
-
-  // const onClickReturn = () => {
-  //   const prevStep = activeStep - 1
-  //   setActiveStep(prevStep > 0 ? prevStep : 0)
-  // }
-
-  let data = questions.data.definition.data.clusters;
+function index() {
   return (
     <div>
-      <Start />
-      {/* <Funktionen /> */}
-      {/* <div className="actions">
-        <Button onClick={onClickReturn}>Zurück</Button>
-        <Button
-          className="home-continue-button"
-          style={{
-            marginLeft: '15px',
-            background: '#2C88FF',
-            border: '1px solid #2C88FF',
-          }}
-          onClick={onClickContinue}
-          variant="primary"
-        >
-          Weiter
-        </Button>
-      </div> */}
+      <RadioButton />
+
+      {/* <SelectButton /> */}
     </div>
-  );
+  )
 }
 
-export default Question;
+export default index
--- src/components/start/index.jsx
+++ src/components/start/index.jsx
@@ -1,54 +1,54 @@
-import React, { useState, useEffect } from "react";
-import { Button, Row, Col, Layout } from "antd";
+import React, { useState, useEffect } from 'react'
+import { Button, Row, Col, Layout } from 'antd'
 import {
   ArrowDownOutlined,
   ArrowUpOutlined,
   UpOutlined,
   DownOutlined,
-} from "@ant-design/icons";
-import "./styles.css";
-import questions from "../../Questions.json";
-import { Radio } from "antd";
+} from '@ant-design/icons'
+import './styles.css'
+import questions from '../../Questions.json'
+import { Radio } from 'antd'
 
 function Start() {
-  const { Footer } = Layout;
-  const [dataIndex, setdataIndex] = useState(0);
-  const [answer, setanswer] = useState([]);
-  let data = questions.data.definition.data.clusters;
+  const { Footer } = Layout
+  const [dataIndex, setdataIndex] = useState(0)
+  const [answer, setanswer] = useState([])
+  let data = questions.data.definition.data.clusters
 
   useEffect(() => {
     let da = data[0].nodes.map((d) => {
-      return { name: d.name, id: d.id };
-    });
-    setanswer(da);
-  }, [data]);
+      return { name: d.name, id: d.id }
+    })
+    setanswer(da)
+  }, [data])
 
   // console.log(`answer[dataIndex].answer::::`, answer[dataIndex].answer);
   return (
     <div>
       <Row>
         <Col span={24} className="start_title">
-          <div style={{ marginLeft: "20%", marginRight: "20%" }}>
+          <div style={{ marginLeft: '20%', marginRight: '20%' }}>
             <h2 className="question">{data[0].nodes[dataIndex].name}</h2>
             <h3>{data[0].nodes[dataIndex].description}</h3>
-            <div style={{ marginTop: "20px", marginBottom: "20px" }}>
+            <div style={{ marginTop: '20px', marginBottom: '20px' }}>
               {data[0].nodes[dataIndex] &&
               data[0].nodes[dataIndex].block &&
               data[0].nodes[dataIndex].block.type &&
-              data[0].nodes[dataIndex].block.type === "tripetto-block-scale" ? (
+              data[0].nodes[dataIndex].block.type === 'tripetto-block-scale' ? (
                 <Radio.Group
                   defaultValue="a"
                   size="large"
                   value={answer[dataIndex] && answer[dataIndex].answer}
                   buttonStyle="solid"
-                  style={{ color: "red" }}
+                  style={{ color: 'red' }}
                   onChange={(e) => {
-                    let newArray = [...answer];
+                    let newArray = [...answer]
                     let d = newArray.filter(function (x) {
-                      return x.id === data[0].nodes[dataIndex].id;
-                    })[0];
-                    d.answer = e.target.value;
-                    setanswer([...newArray]);
+                      return x.id === data[0].nodes[dataIndex].id
+                    })[0]
+                    d.answer = e.target.value
+                    setanswer([...newArray])
                   }}
                 >
                   {[...Array(11)].map((e, i) => {
@@ -56,41 +56,41 @@ function Start() {
                       <Radio.Button className="radio-tab" value={i}>
                         {i}
                       </Radio.Button>
-                    );
+                    )
                   })}
                 </Radio.Group>
               ) : null}
             </div>
 
-            <div style={{ marginTop: "20px", marginBottom: "20px" }}>
+            <div style={{ marginTop: '20px', marginBottom: '20px' }}>
               {data[0].nodes[dataIndex] &&
               data[0].nodes[dataIndex].block &&
               data[0].nodes[dataIndex].block.type &&
               data[0].nodes[dataIndex].block.type ===
-                "tripetto-block-multiple-choice" ? (
+                'tripetto-block-multiple-choice' ? (
                 <div
-                  style={{ display: "inline-flex", flexDirection: "column" }}
+                  style={{ display: 'inline-flex', flexDirection: 'column' }}
                 >
                   {data[0].nodes[dataIndex].block.choices.map((u, i) => {
                     return (
                       <button
                         onClick={() => {
-                          let newArray = [...answer];
+                          let newArray = [...answer]
                           let d = newArray.filter(function (x) {
-                            return x.id === data[0].nodes[dataIndex].id;
-                          })[0];
-                          d.answer = u.name;
-                          setanswer([...newArray]);
+                            return x.id === data[0].nodes[dataIndex].id
+                          })[0]
+                          d.answer = u.name
+                          setanswer([...newArray])
                         }}
                         className={
                           answer[dataIndex].answer === u.name
-                            ? "active-radio-multiple-choice"
-                            : "radio-multiple-choice"
+                            ? 'active-radio-multiple-choice'
+                            : 'radio-multiple-choice'
                         }
                       >
                         {u.name}
                       </button>
-                    );
+                    )
                   })}
                 </div>
               ) : null}
@@ -102,7 +102,7 @@ function Start() {
                 icon={<ArrowDownOutlined />}
                 onClick={() => {
                   if (dataIndex < data[0].nodes.length - 1) {
-                    setdataIndex(dataIndex + 1);
+                    setdataIndex(dataIndex + 1)
                   }
                 }}
               >
@@ -120,16 +120,16 @@ function Start() {
               <Button
                 onClick={() => {
                   if (dataIndex > 0) {
-                    setdataIndex(dataIndex - 1);
+                    setdataIndex(dataIndex - 1)
                   }
                 }}
-                style={{ marginRight: "20px" }}
+                style={{ marginRight: '20px' }}
                 icon={<UpOutlined />}
               ></Button>
               <Button
                 onClick={() => {
                   if (dataIndex < data[0].nodes.length - 1) {
-                    setdataIndex(dataIndex + 1);
+                    setdataIndex(dataIndex + 1)
                   }
                 }}
                 icon={<DownOutlined />}
@@ -139,7 +139,7 @@ function Start() {
         </Layout>
       </Row>
     </div>
-  );
+  )
 }
 
-export default Start;
+export default Start
